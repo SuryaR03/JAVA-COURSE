01ddf94ab591fb136e281bb7ced3e26ef71a6591
SuryaR03 <suryarajendran0713@gmail.com>
2025-09-10 01:03:36 +0530

    Learned upto Ifelse

---
+++ BASICS/if-else.java
@@ -0,0 +1,14 @@
+
+class hello{
+    public static void main(String args[])
+    {
+        int a=20;
+        int b=30;
+
+        if(a<b){
+            System.out.println("a is the great number");
+        }else{
+            System.out.print("b is the greater");
+        }
+    }
+}
