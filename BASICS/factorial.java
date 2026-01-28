d88b44a9ce1301e3cd93e8be4a8671a08130d4b1
SuryaR03 <suryarajendran0713@gmail.com>
2026-01-28 23:58:30 +0530

    updated

---
+++ BASICS/factorial.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class factorial{
+    public static int factorial(int n ){
+        
+        if(n==0){
+            return 1;
+        }
+        return n * factorial(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int result=factorial(n);
+        System.out.println(result);
+
+    }
+}
