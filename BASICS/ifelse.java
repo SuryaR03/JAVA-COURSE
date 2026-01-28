5ffc29421e2d499779aaf63d76df2177b11b57c1
SuryaR03 <suryarajendran0713@gmail.com>
2026-01-28 22:51:49 +0530

    updated

--- BASICS/ifelse.java
+++ BASICS/ifelse.java
@@ -4,13 +4,13 @@ class ifelse{
     public static void main(String args[])
     {
         Scanner scan= new Scanner(System.in);
-        int mark =scan.nextInt();
+        int num =scan.nextInt();
 
 
-        if(mark<40){
-            System.out.println("Fail");
+        if(num%3==0 && num%5==0){
+            System.out.println("div by 3 and 5");
         }else{
-            System.out.print("Pass");
+            System.out.print("not div by 3 and 5");
         }
     }
 }
