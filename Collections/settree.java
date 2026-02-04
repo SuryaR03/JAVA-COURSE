45c0df5138c544528862e3a305f24c8654bfc7a9
SuryaR03 <suryarajendran0713@gmail.com>
2026-02-04 15:44:13 +0530

    updated

---
+++ Collections/settree.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class settree {
+    public static void main(String[] args) {
+
+        TreeSet<Integer> s=new TreeSet<>();
+        s.add(5);
+        s.add(7);
+        s.add(6);
+        s.add(8);
+        s.add(2);
+        System.out.println("Elements in set "+s);
+        System.out.println("Elements in set "+ s.ceiling(4));
+        System.out.println("first value "+s.first());
+        System.out.println("Last value "+s.last());
+        System.out.println("Floor value "+s.floor(3));
+        System.out.println("higher in set "+s.higher(5));
+        System.out.println("Lower in set "+s.lower(5));
+
+        System.out.println("pollfirst in set "+s.pollFirst());
+        System.out.println("polllast in set "+s.pollLast());
+    }
+}
--- Collections/tempCodeRunnerFile.java
+++
@@ -1,5 +0,0 @@
-Iterator<String> itr= list.iterator();
-        // System.out.println();
-        // while(itr.hasNext()){
-        //     String fruit = itr.next();
-        //     System.err.println(fruit);
