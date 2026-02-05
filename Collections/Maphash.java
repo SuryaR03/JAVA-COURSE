f180a654fef81c2ce83b1153785aff9204cc6cb0
SuryaR03 <suryarajendran0713@gmail.com>
2026-02-05 11:47:24 +0530

    updated

--- Collections/Maphash.java
+++ Collections/Maphash.java
@@ -9,6 +9,7 @@ public class Maphash{
         num.put("reg2", 2);
         num.put("reg3", 3);
         num.put("reg4", 4);
+        num.put("reg5", 5);
 
         System.out.println("All reg no :"+num);
 
@@ -23,8 +24,28 @@ public class Maphash{
         System.err.println("Entries: "+ num.containsKey("reg1"));     //boolean
  
         System.err.println("Entries: "+ num.replace("reg2",6));        //replace
-        
-        int value = num.remove("reg3");
-        System.err.println("removed value : "+value);
+
+        System.out.println("first key: "+ num.firstKey());           //return first 
+
+        System.out.println("last key: "+ num.lastKey());          //retrun last
+
+        System.out.println("first enrty: "+ num.firstEntry());          //with value
+
+        System.out.println("last Entry: "+ num.lastEntry());
+
+        System.out.println("higher key: "+ num.higherKey("reg1"));         //strict greater
+        System.out.println("higher Entry: "+ num.higherEntry("reg1"));
+
+        System.out.println("lower key: "+ num.lowerKey("reg2"));           //strict lower
+        System.out.println("lower entry: "+ num.lowerEntry("reg2"));
+
+        System.out.println("ceiling key: "+ num.ceilingKey("reg4"));      //greater than or equal
+        System.out.println("ceiling entry: "+ num.ceilingKey("reg4"));
+
+        System.out.println("floor key: "+ num.floorKey("reg3"));           //less than or equal
+        System.out.println("floor entry: "+ num.floorKey("reg3"));
+
+        System.out.println("poll first entry: "+ num.pollFirstEntry());     //remove and return 
+        System.out.println("poll last entry: "+ num.pollLastEntry());
     }
 }
