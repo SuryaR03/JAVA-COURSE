a7e695cbade432d26747a083cb1db3f428290132
SuryaR03 <suryarajendran0713@gmail.com>
2026-02-05 11:06:10 +0530

    updated

---
+++ Collections/Maphash.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Maphash{
+    public static void main(String[] args) {
+        
+        TreeMap<String,Integer> num= new TreeMap<>();
+
+        num.put("reg1", 1);
+        num.put("reg2", 2);
+        num.put("reg3", 3);
+        num.put("reg4", 4);
+
+        System.out.println("All reg no :"+num);
+
+        System.out.println("key :"+num.keySet());           //key value
+
+        System.err.println("values :"+num.values());         //values
+
+        System.err.println("Entries: "+ num.entrySet());        //key with values
+
+        System.err.println("get the value: "+ num.get("reg2"));        //specific value of the key given
+
+        System.err.println("Entries: "+ num.containsKey("reg1"));     //boolean
+ 
+        System.err.println("Entries: "+ num.replace("reg2",6));        //replace
+        
+        System.err.println("Entries: "+ num.keySet());
+        
+        System.err.println("Entries: "+ num.values());
+
+        int value = num.remove("reg3");
+        System.err.println("removed value : "+value);
+    }
+}
